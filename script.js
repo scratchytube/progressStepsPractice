0695a0b69d9971f16e6125fcfd46c84214eae954
Brian Sahota <bpsahota@gmail.com>
2021-04-17 15:11:16 -0400

    javascript

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ const progress = document.querySelector('#progress')
 const prev = document.querySelector('#prev')
 const next = document.querySelector('#next')
 const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+const update = () => {
+    circles.forEach((circle, idx) => {
+        
+    })
+}
